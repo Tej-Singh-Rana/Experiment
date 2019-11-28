a3f615b758a19370f9f9040309fac77ec577af7e
root <root@security2u.com>
2019-11-28 15:57:27 +0530

    condition

---
+++ if.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+a=2
+b=3
+c=2
+d=12
+if a>3:
+
+  print("this is true statement")
+
+elif a == b:
+ print("wrong statement")
+
+elif a == c:
+
+  print("true statement")
+
+else:
+
+ print("everything over")
+
+
+
