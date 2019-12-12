2c8a16d7f37305fc6c0a84dcf73ee4918ef4d957
root <root@station197.example.com>
2019-12-12 14:50:45 +0530

    practice

---
+++ multiple-parameter.py
@@ -0,0 +1,12 @@
+def number(cat1,cat2,cat3):
+    print("printing a value of three : ")
+    print("Value of one : ",cat1)
+    print("Value of two : ",cat2)
+    print("Value of three : ",cat3)
+    z=cat1+cat2+cat3
+    print(z)
+number(5,3,6)
+number(10,20,30)
+number(12,20,15)
+number(cat3=10,cat2=5,cat1=1)
+number(15,20,50)
